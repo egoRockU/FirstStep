819894c2977ca4713c80db7694503691f520127d
Ranpooooo <148606339+Ranpooooo@users.noreply.github.com>
2024-02-12 16:03:32 +0800

    Update Working

--- backend22/src/services/Update.js
+++ backend22/src/services/Update.js
@@ -2,13 +2,11 @@ const ApplicantProfile = require('../models/ApplicantProfile');
 
 module.exports = async (_id, set) => {
   try {
-    
     await ApplicantProfile.updateOne({ _id }, { $set: set });
-
+    
     return true;
   } catch (err) {
-    console.log("err: ", err);
+    console.error("Error updating ApplicantProfile:", err);
     return false;
   }
 };
-/* sira pa tong update pala ger 'di ko pa naayos */
