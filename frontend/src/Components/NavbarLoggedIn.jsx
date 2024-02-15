ab236b0db5301d7cb4d92285a9a3183ab86aef3a
unknown <buenafe.micholojoaquin.bscs2021@gmail.com>
2024-02-16 01:07:30 +0800

    new create profile but not done

--- frontend/src/Components/NavbarLoggedIn.jsx
+++ frontend/src/Components/NavbarLoggedIn.jsx
@@ -24,50 +24,18 @@ function NavbarLoggedIn() {
   }
 
   return (
-    <nav className='p-3 bg-white bg-opacity-75 flex shadow-xl justify-between items-center'>
-      <div className="flex items-center">
-        <button
-          className="block lg:hidden focus:outline-none ml-3"
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Toggle Menu"
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            {isOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            )}
-          </svg>
-        </button>
-        {/* Render logo based on isOpen state */}
-        {!isOpen && (
-          <img src={logo} alt="logo" className='h-12 transition-transform hover:scale-125 cursor-pointer' onClick={clickLanding}/>
-        )}
-      </div>
-      <div className={`lg:flex lg:items-center space-x-4 ${isOpen ? 'block w-full lg:w-auto' : 'hidden'}`}>
-        <ul className="lg:flex lg:items-center">
-          <li className='my-6 lg:my-0 lg:mx-4 text-xl hover:text-stone-400'>Resume Builder</li>
-          <li className='my-6 lg:my-0 lg:mx-4 text-xl hover:text-stone-400'>Portfolio Builder</li>
-          <li className='my-6 lg:my-0 lg:mx-4 text-xl hover:text-stone-400'>About us</li>
-          <li className='my-6 lg:my-0 lg:mx-4 text-xl hover:text-stone-400'>Employees</li>
-        </ul>
-      </div>
-      <div className='flex items-center space-x-5 justify-between'>
+    <nav className='p-1 bg-blue-300'>
+      <div className='flex w-full'>
+     <div className='flex items-center sm:w-1/2 lg:w-full hover:font-semibold' onClick={clickLanding}>
+      <img src={logo} alt="" className='w-12 h-12' />
+      <h1 className='text-xl'>FirstStep</h1>
+     </div>
+     <div className='w-full flex justify-around'>
+      <ul className='flex h-full items-center space-x-10'>
+        <li className='text-lg font-medium'>Resume Builder</li>
+        <li className='text-lg font-medium'>Portfolio Builder</li>
+      </ul>
+      <div className='flex items-center space-x-5'>
         {/* Notification icon */}
         <BsBell className="text-black duration-500 mx-2 cursor-pointer text-3xl" />
         {/* Profile icon */}
@@ -76,7 +44,8 @@ function NavbarLoggedIn() {
         {/* Logout button */}
         <button className='bg-[#CB8A8A] text-white duration-500 px-3 py-2 mx-2 rounded-lg' onClick={clickLogout}>Logout</button>
       </div>
-
+     </div>
+     </div>
     </nav>
   );
 }
--- frontend/src/Pages/Create.jsx
+++ frontend/src/Pages/Create.jsx
@@ -1,101 +1,155 @@
-import React from "react";
+import { useState } from "react";
 import Man from "../images/tommy.png";
 import banner from "../images/signBg.jpg";
 import NavbarLoggedIn from "../Components/NavbarLoggedIn";
 import google from "../images/google.png";
+import Footer from "../Components/Footer";
+import imgph from "../images/imgplaceholder.png";
 
 function Create() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const placeholderImage = "placeholder.jpg"; // Provide your placeholder image URL here
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        setSelectedImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <>
       <NavbarLoggedIn />
-      <div className="container mx-auto mt-2">
-        <img
-          src={banner}
-          alt=""
-          className="w-full h-80 object-cover rounded-lg"
-        />
-      </div>
-      <div className="border-2 rounded-lg flex container mx-auto mb-28 shadow-lg">
-        <div className="w-1/3 p-4 flex flex-col items-start">
-          <div>
-            <img
-              src={Man}
-              alt=""
-              className="w-60 h-60 rounded-full border-4 border-white mb-4 md:mb-0 md:mr-4"
-            />
-          </div>
-          <div>
-            <h1 className="text-3xl font-semibold">John Doe</h1>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-              euismod nunc a purus varius convallis.
-            </p>
-          </div>
-          <div className="flex flex-col items-start space-y-4">
-            <div className="flex items-center space-x-2">
-              <img src={google} alt="Facebook" className="w-8 h-8" />
-              <span>Facebook</span>
-            </div>
-            <div className="flex items-center space-x-2">
-              <img src={google} alt="Twitter" className="w-8 h-8" />
-              <span>Twitter</span>
-            </div>
-            <div className="flex items-center space-x-2">
-              <img src={google} alt="Instagram" className="w-8 h-8" />
-              <span>Instagram</span>
-            </div>
-          </div>
-          <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-            <div className="flex items-center justify-center mb-2">
-              <div className="w-32 h-16 rounded-full flex items-center justify-center">
-                <span className="text-small font-medium">Preferred Jobs</span>
+      <div className="relative">
+        <div
+          style={{
+            background: "linear-gradient(to bottom, #ffffff,#87ceeb)",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: -1,
+          }}
+        ></div>
+        <div className="h-full relative">
+          <img
+            src={banner}
+            alt=""
+            className="w-full h-80 object-cover rounded-xl"
+          />
+        </div>
+        <div className="w-full flex mb-20">
+          <div className=" bg-white md:left-[100px] lg:left-[192px] mt-[-40px] absolute w-2/5 lg:w-1/5 rounded-2xl">
+            <div className="p-4 flex flex-col items-center">
+              <div style={{ position: "relative", display: "inline-block" }}>
+                <input
+                  type="file"
+                  id="imageInput"
+                  accept="image/*"
+                  style={{ display: "none" }}
+                  onChange={handleImageChange}
+                />
+                <label htmlFor="imageInput" className="cursor-pointer">
+                  <img
+                    src={selectedImage || placeholderImage}
+                    alt=""
+                    className="w-60 h-60 rounded-full border-4 border-white mb-4 md:mb-0 md:mr-4"
+                  />
+                </label>
+                {!selectedImage && (
+                  <div
+                    onClick={() =>
+                      document.getElementById("imageInput").click()
+                    }
+                    className="absolute inset-0 cursor-pointer"
+                    style={{ zIndex: 1 }}
+                  >
+                    <img
+                      src={imgph}
+                      alt="Placeholder"
+                      className="w-60 h-60 rounded-full"
+                    />
+                  </div>
+                )}
+              </div>
+              <div>
+                <h1 className="text-3xl font-semibold">John Doe</h1>
+                <p className="text-gray-700">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  euismod nunc a purus varius convallis.
+                </p>
+              </div>
+              <div className="flex items-start space-y-4">
+                <div className="items-center space-x-2">
+                  <img src={google} alt="Facebook" className="w-8 h-8" />
+                </div>
+                <div className="items-center space-x-2">
+                  <img src={google} alt="Twitter" className="w-8 h-8" />
+                </div>
+                <div className="items-center space-x-2">
+                  <img src={google} alt="Instagram" className="w-8 h-8" />
+                </div>
+              </div>
+              <div className="bg-gray-100 p-3 rounded-lg shadow-md">
+                <div className="flex items-center justify-center mb-2">
+                  <div className="w-32 h-16 rounded-full flex items-center justify-center">
+                    <span className="text-small font-medium">
+                      Preferred Jobs
+                    </span>
+                  </div>
+                </div>
+                <button className="text-2xl bg-[#FFCECE] w-full rounded-full">
+                  +
+                </button>
               </div>
             </div>
-            <button className="text-2xl bg-[#FFCECE] w-full rounded-full">
-              +
-            </button>
+          </div>
+          <div className="flex flex-col space-y-8">
+            <form className="md:w-[430px] lg:w-[1000px] h-[300px] mt-[-40px] rounded-xl p-2 bg-white relative md:left-[500px] lg:left-[600px]">
+              <div className="w-full ">
+                <div className="flex justify-between mx-5 items-center">
+                  <h2 className="text-3xl font-semibold mb-4">Summary</h2>
+                  <button className="text-2xl">+</button>
+                </div>
+              </div>
+            </form>
+            <div className="relative md:w-[430px] lg:w-[1000px] h-[300px] rounded-xl p-2 bg-white md:left-[500px] lg:left-[600px]">
+              <div className="flex justify-between mx-5 items-center">
+                <h3 className="text-3xl font-semibold mb-2 ">
+                  Academic Achievements
+                </h3>
+                <button className="text-2xl">+</button>
+              </div>
+            </div>
+            <div className="relative md:w-[430px] lg:w-[1000px] h-[300px] rounded-xl p-2 bg-white md:left-[500px] lg:left-[600px]">
+              <div className="flex justify-between mx-5 items-center">
+                <h3 className="text-3xl font-semibold mb-2 ">
+                  Activities and Inovements
+                </h3>
+                <button className="text-2xl">+</button>
+              </div>
+            </div>
+            <div className="relative md:w-[430px] lg:w-[1000px] h-[300px] rounded-xl p-2 bg-white md:left-[500px] lg:left-[600px]">
+              <div className="flex justify-between mx-5 items-center">
+                <h3 className="text-3xl font-semibold mb-2 ">Projects</h3>
+                <button className="text-2xl">+</button>
+              </div>
+            </div>
+            <div className="relative md:w-[430px] lg:w-[1000px] h-[200px] rounded-xl p-2 bg-white md:left-[500px] lg:left-[600px]">
+              <div className="flex justify-between mx-5 items-center">
+                <h3 className="text-3xl font-semibold mb-2 ">Certificates</h3>
+                <button className="text-2xl">+</button>
+              </div>
+            </div>
+          </div>
         </div>
-          </div>
-        <form className="w-2/3">
-          <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-            <div>
-              <h2 className="text-3xl font-semibold mb-4">Summary</h2>
-              <textarea
-              class="w-full h-full bg-transparent border-none outline-none p-4 rounded-xl resize-none text-xl"
-              placeholder="Type here..."
-            ></textarea>
-            </div>
-            <hr className="w-full border-gray-300 my-4" />
-            <div className="mb-4">
-              <h3 className="text-xl font-semibold mb-2">Academic Achievements</h3>
-              <input
-                type="file"
-                accept="image/*"
-                className="border rounded-lg p-2 w-full"
-              />
-            </div>
-            <hr className="w-full border-gray-300 my-4" />
-            <div className="mb-4">
-              <h3 className="text-xl font-semibold mb-2">Projects</h3>
-              <input type="file" className="border rounded-lg p-2 w-full" />
-            </div>
-            <hr className="w-full border-gray-300 my-4" />
-            <div>
-              <h3 className="text-xl font-semibold mb-2">
-                Certificate Achievements
-              </h3>
-              <input type="file" className="border rounded-lg p-2 w-full" />
-            </div>
-            <hr className="w-full border-gray-300 my-4" />
-            <button
-              type="submit"
-              className="bg-[#FFCECE] text-black py-2 px-4 rounded-lg shadow-md hover:text-stone-500"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
       </div>
+      <Footer />
     </>
   );
 }
