8aa6da543324870df0a3c05e36b0cb0ae13b232d
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2024-05-15 15:36:22 +0800

    added some placeholders

--- frontend/src/Modals/Edit Profile/Addcertificates.jsx
+++ frontend/src/Modals/Edit Profile/Addcertificates.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { IoCloseOutline } from "react-icons/io5";
 import { ImSpinner } from "react-icons/im";
 
-
 function AddCertificates({
   onClose,
   onSubmit,
@@ -20,7 +19,6 @@ function AddCertificates({
 
   const [imagePreview, setImagePreview] = useState(null);
   const [submitting, setSubmitting] = useState(false); // State to track submission process
-
 
   useEffect(() => {
     if (initialData) {
@@ -43,10 +41,10 @@ function AddCertificates({
     e.preventDefault();
     setSubmitting(true);
     setTimeout(async () => {
-      await onSubmit(formData); 
-      setSubmitting(false); 
+      await onSubmit(formData);
+      setSubmitting(false);
       onClose();
-    }, 1000); 
+    }, 1000);
   };
 
   const handleEdit = (e) => {
@@ -75,8 +73,8 @@ function AddCertificates({
     <div className="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50">
       <div className="bg-white p-5 rounded-xl w-1/3">
         <div className="flex justify-between w-full">
-        <h2 className="text-xl text-[#444B88]">Add Certificates</h2>
-        <IoCloseOutline size={25} onClick={handleCancel} />
+          <h2 className="text-xl text-[#444B88]">Add Certificates</h2>
+          <IoCloseOutline size={25} onClick={handleCancel} />
         </div>
         <form onSubmit={handleSubmit} className="flex flex-col gap-2">
           <div>
@@ -113,29 +111,7 @@ function AddCertificates({
               required
             />
           </div>
-          {/*
-          <div>
-            <label htmlFor="image" className="block text-[#444B88]">
-              Image:
-            </label>
-            <input
-              type="file"
-              id="image"
-              name="image"
-              onChange={handleChange}
-              className="border border-[#444B88] rounded-md px-2 py-2 w-full"
-            />
-            </div>
-          <div>
-            {imagePreview && (
-              <img
-                src={imagePreview}
-                alt="Selected Image"
-                className="max-w-full h-auto"
-              />
-            )}
-          </div>
-          */}
+
           <div className="mb-4">
             <label htmlFor="description" className="block text-[#444B88]">
               Description:
@@ -145,6 +121,7 @@ function AddCertificates({
               name="description"
               value={formData.description}
               onChange={handleChange}
+              placeholder="Add more information about this certificate."
               className="border border-[#444B88] rounded-md px-2 py-2 w-full"
               required
               rows={8}
@@ -160,26 +137,33 @@ function AddCertificates({
             </button>
 
             {!initialData && (
-  <button
-    type="submit"
-    disabled={submitting}
-    className="bg-[#8B95EE] border border-[#444B88] text-white px-4 py-2 rounded-md flex justify-center items-center gap-2"
-  >
-    {submitting ? <ImSpinner className="animate-spin mr-2" /> : "Submit"}
-  </button>
-)}
+              <button
+                type="submit"
+                disabled={submitting}
+                className="bg-[#8B95EE] border border-[#444B88] text-white px-4 py-2 rounded-md flex justify-center items-center gap-2"
+              >
+                {submitting ? (
+                  <ImSpinner className="animate-spin mr-2" />
+                ) : (
+                  "Submit"
+                )}
+              </button>
+            )}
 
-{initialData && (
-  <button
-    type="button"
-    onClick={handleEdit}
-    disabled={submitting}
-    className="bg-[#8B95EE] border border-[#444B88] text-black px-4 py-2 rounded-md mr-2"
-  >
-    {submitting ? <ImSpinner className="animate-spin mr-2" /> : "Save Changes"}
-  </button>
-)}
-
+            {initialData && (
+              <button
+                type="button"
+                onClick={handleEdit}
+                disabled={submitting}
+                className="bg-[#8B95EE] border border-[#444B88] text-black px-4 py-2 rounded-md mr-2"
+              >
+                {submitting ? (
+                  <ImSpinner className="animate-spin mr-2" />
+                ) : (
+                  "Save Changes"
+                )}
+              </button>
+            )}
           </div>
         </form>
         <button
@@ -202,7 +186,6 @@ function AddCertificates({
         </button>
       </div>
     </div>
-    
   );
 }
 
