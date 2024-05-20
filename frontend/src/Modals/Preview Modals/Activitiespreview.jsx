735968b7c7e5461a08271f36be36f31ee5d92db4
Jay Ann Rose Gerente <gerente.jayannrose.bscs2021@gmail.com>
2024-05-20 17:34:27 +0800

    responsive modals

--- frontend/src/Modals/Preview Modals/Activitiespreview.jsx
+++ frontend/src/Modals/Preview Modals/Activitiespreview.jsx
@@ -14,22 +14,22 @@ function Activitiespreview({ onClose, activity }) {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50 py-5">
       <div className="flex flex-col justify-center items-center w-full h-full ">
-        <div className="w-1/3 bg-white shadow-lg">
+        <div className="w-4/5 md:w-1/3 bg-white shadow-lg rounded-lg">
           <div className="w-full flex justify-end py-2 px-2 border border-gray-300">
             <IoMdClose size={30} onClick={onClose} />
           </div>
           <div className="border border-gray-300 flex flex-col px-4 py-2 gap-1">
             <div className="flex gap-4">
-              <h1 className="text-2xl text-[#444b88] font-bold">{title}</h1>
-              <div className="flex gap-3 text-lg items-center">
+              <h1 className="text-base md:text-xl text-[#444b88] font-semibold">{title}</h1>
+              <div className="flex gap-3 text-sm md:text-lg items-center">
                 <h1>{convertDateYear(startDate)}</h1>
                 <p>-</p>
                 <h1>{convertDateYear(endDate)}</h1>
               </div>
             </div>
-            <h1 className="text-lg">{organizationOrCompanyName}</h1>
-            <p className="italic text-lg text-gray-400">{location}</p>
-            <p className="text-lg">{description}</p>
+            <h1 className="text-sm md:text-lg">{organizationOrCompanyName}</h1>
+            <p className="italic text-sm md:text-lg text-gray-400">{location}</p>
+            <p className="text-sm md:text-lg">{description}</p>
           </div>
         </div>
       </div>
--- frontend/src/Modals/Preview Modals/Awardspreview.jsx
+++ frontend/src/Modals/Preview Modals/Awardspreview.jsx
@@ -7,16 +7,16 @@ function Awardspreview({ onClose, award }) {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-50 py-5">
       <div className="flex flex-col justify-center items-center w-full h-full ">
-        <div className="w-1/3 bg-white shadow-lg">
+        <div className="w-3/4 md:w-1/3 bg-white shadow-lg rounded-lg">
           <div className="w-full flex justify-end py-2 px-2 border border-gray-300">
             <IoMdClose size={30} onClick={onClose} />
           </div>
           <div className="flex flex-col border border-gray-300 p-3 items-center gap-2 pb-10">
-            <p className="text-2xl text-[#444b88] font-bold">{title}</p>
-            <p className="text-lg italic text-gray-400">
+            <p className="text-base md:text-lg text-[#444b88] font-bold">{title}</p>
+            <p className="text-sm md:text-lg italic text-gray-400">
               {convertDate(dateReceived)}
             </p>
-            <p className="text-lg">{description}</p>
+            <p className="text-sm md:text-lg text-justify text-center">{description}</p>
           </div>
         </div>
       </div>
